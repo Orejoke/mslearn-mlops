ed2e748ade0186c5b0f977dfb54a281158893f18
Oren Berkovich <69391543+Orejoke@users.noreply.github.com>
2023-08-09 19:12:18 +0300

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -12,7 +12,7 @@ from sklearn.linear_model import LogisticRegression
 # define functions
 def main(args):
     # TO DO: enable autologging
-
+    mlflow.autolog()
 
     # read data
     df = get_csvs_df(args.training_data)
@@ -34,7 +34,16 @@ def get_csvs_df(path):
 
 
 # TO DO: add function to split data
+def split_data(self, df):
+    # split data
+    X = df.drop(columns=["target"])
+    y = df["target"]
 
+    # split data
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+    # return split data
+    return X_train, X_test, y_train, y_test
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
